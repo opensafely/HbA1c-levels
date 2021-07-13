32098fed105b88e4fa2284d91680eacd0f843ca9
robinyjpark <72387969+robinyjpark@users.noreply.github.com>
2021-07-13 11:03:29 +0100

    Add ethnicity

--- analysis/join_ethnicity.py
+++ analysis/join_ethnicity.py
@@ -9,6 +9,6 @@ for file in os.listdir('output/data'):
         if file.split('_')[1] not in ['ethnicity.csv']:
             file_path = os.path.join('output/data', file)
             df = pd.read_csv(file_path)
-            merged_df = df.merge(ethnicity_df, how='left', on='patient_id')
+            merged_df = df.merge(ethnicity_df, how='left', on='patient_id').set_index('patient_id')
             
             merged_df.to_csv(file_path)
