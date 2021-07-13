38c950bc7ec5ebecdcd5c0a99acd9526b8b5a7df
robinyjpark <72387969+robinyjpark@users.noreply.github.com>
2021-07-13 11:11:14 +0100

    Update filepath

--- analysis/join_ethnicity.py
+++ analysis/join_ethnicity.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import os
 
-ethnicity_df = pd.read_csv('output/data/input_ethnicity.csv')
+ethnicity_df = pd.read_csv('../output/data/input_ethnicity.csv')
 
-for file in os.listdir('output/data'):
+for file in os.listdir('../output/data'):
     if file.startswith('input'):
         #exclude ethnicity
         if file.split('_')[1] not in ['ethnicity.csv']:
-            file_path = os.path.join('output/data', file)
+            file_path = os.path.join('../output/data', file)
             df = pd.read_csv(file_path)
             merged_df = df.merge(ethnicity_df, how='left', on='patient_id').set_index('patient_id')
             
